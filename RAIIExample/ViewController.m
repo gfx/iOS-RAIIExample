5b68b3830d2a539367a869ec26b561a01d4605ac
Fuji, Goro <goro-fuji@cookpad.com>
2013-12-05 17:57:45 +0900

    impl

--- RAIIExample/ViewController.m
+++ RAIIExample/ViewController.m
@@ -8,6 +8,53 @@
 
 #import "ViewController.h"
 
+@interface NetworkActivityGuard : NSObject
+@end
+
+@implementation NetworkActivityGuard
+
+- (id)init
+{
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    return [super init];
+}
+
+- (void)dealloc
+{
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+}
+
+@end
+
+@interface ScopeGuard : NSObject
+@end
+
+typedef void (^voidBlock)();
+
+@implementation ScopeGuard {
+    voidBlock _block;
+}
+
+- (id)initWithBlock: (voidBlock)block
+{
+    self = [super init];
+    self->_block = [block copy];
+    return self;
+}
+
+- (void)dealloc
+{
+    _block();
+}
+
++(id)newWitBlock:(voidBlock)block
+{
+    return [[self alloc] initWithBlock:block];
+}
+
+@end // ScopeGuard
+
+
 @interface ViewController ()
 
 @end
@@ -17,13 +64,51 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// good
+- (IBAction)startAction:(id)sender {
+    NSLog(@"started");
+
+    self.label.text = @"Working In Progress";
+    
+    NSMutableArray * const guards = [NSMutableArray new];
+    [guards addObject: [ScopeGuard newWitBlock:^{
+        self.label.text = @"Hello, world!";
+    }]];
+    [guards addObject:[NetworkActivityGuard new]];
+
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    
+    double const delayInSeconds = 1.5;
+    dispatch_time_t const popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        NSLog(@"finished");
+        if (YES) {
+            // no need to release guards!
+            return;
+        }
+        
+        (void)guards;
+    });
 }
+
+
+
+
+// bad
+- (IBAction)_startAction:(id)sender {
+    self.label.text = @"Working In Progress";
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    
+    double const delayInSeconds = 1.5;
+    dispatch_time_t const popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        
+        self.label.text = @"Hello, world!";
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    });
+}
+
 
 @end
